472eb295c2db7007822b5e66c514fa50459af0a1
Stijn Van Bael <stijn.vanbael@sweetmustard.be>
2018-09-03 21:29:35 +0200

    Improved Indra CLI

--- bin/run.dart
+++ bin/run.dart
@@ -2,9 +2,20 @@ library indra.daemon;
 
 import 'dart:io';
 
+import 'package:ansicolor/ansicolor.dart';
 import 'package:indra/src/runner.dart';
 
-main(List<String> arg) {
-  runScript('${Directory.current.path}/${arg[0]}', arg.sublist(1),
-      new RunnerControl());
+var red = new AnsiPen()..red(bold: true);
+
+main(List<String> args) {
+  if (args.isEmpty) {
+    print(red('Usage: indra <script>'));
+  } else {
+    var script = args[0];
+    if (!script.endsWith('.dart')) {
+      script = '$script.dart';
+    }
+    runScript('${Directory.current.path}/${script}', args.sublist(1),
+        new RunnerControl());
+  }
 }
