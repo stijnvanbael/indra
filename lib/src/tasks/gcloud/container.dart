52503413f58a8b64efdf0aa224e004b268c02824
Stijn Van Bael <stijn.vanbael@sweetmustard.be>
2019-09-03 11:55:58 +0200

    Added gcloud container images list

--- lib/src/tasks/gcloud/container.dart
+++ lib/src/tasks/gcloud/container.dart
@@ -25,6 +25,16 @@ class Images {
         .where((tag) => tag != null)
         .toList();
   }
+
+  Future<List<Image>> list() async {
+    var params = ['container', 'images', 'list', '--repository=${gcloud.hostname}/${gcloud.project}'];
+    return (await gcloud.run(params, showOutput: false))
+        .split('\n')
+        .skip(1)
+        .map(Image.parse)
+        .where((image) => image != null)
+        .toList();
+  }
 }
 
 class Tag {
@@ -37,6 +47,23 @@ class Tag {
     var parts = tagLine.split(new RegExp(r'\s+'));
     if (parts.length == 3) {
       return Tag(parts[1], DateTime.parse(parts[2]));
+    } else {
+      return null;
+    }
+  }
+}
+
+class Image {
+  String hostname;
+  String project;
+  String name;
+
+  Image(this.hostname, this.project, this.name);
+
+  static Image parse(String imageLine) {
+    var parts = imageLine.split('/');
+    if (parts.length == 3) {
+      return Image(parts[0], parts[1], parts[2]);
     } else {
       return null;
     }
